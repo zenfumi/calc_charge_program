8ba2886c9ca1ca054ad8ac4e7171a1dab79710ff
zenfumi <ys00878@gmail.com>
2021-03-26 00:23:35 +0900

    リファクタリング

--- lib/basic_charge.rb
+++ lib/basic_charge.rb
@@ -14,7 +14,7 @@ class Basic_charge
 
   # 以下、共通化すること
   def self.import2(path)
-    CSV.read("csv/tokyogas/basic_charge.csv", headers: true).map do |row|
+    CSV.read("csv/loop/basic_charge.csv", headers: true).map do |row|
       {
         amp: row["amp"],
         basic_charge: row["basic_charge"]
@@ -23,7 +23,7 @@ class Basic_charge
   end
 
   def self.import3(path)
-    CSV.read("csv/loop/basic_charge.csv", headers: true).map do |row|
+    CSV.read("csv/tokyogas/basic_charge.csv", headers: true).map do |row|
       {
         amp: row["amp"],
         basic_charge: row["basic_charge"]
--- lib/usage_charge.rb
+++ lib/usage_charge.rb
@@ -6,7 +6,8 @@ class Usage_charge
     #全てを読込、配列を作成
     CSV.read("csv/tokyo_energy_partner/usage_charge.csv", headers: true).map do |row|
       {
-        kwh: row["kwh"],
+        kwh_from: row["kwh_from"],
+        kwh_to: row["kwh_to"],
         unit_price: row["unit_price"]
       }
     end
@@ -26,7 +27,8 @@ class Usage_charge
     #全てを読込、配列を作成
     CSV.read("csv/tokyogas/usage_charge.csv", headers: true).map do |row|
       {
-        kwh: row["kwh"],
+        kwh_from: row["kwh_from"],
+        kwh_to: row["kwh_to"],
         unit_price: row["unit_price"]
       }
     end
